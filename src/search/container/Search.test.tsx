a2a479493a6a4b032fe94587d0b651155a3fed23
psajo <psajo@naver.com>
2021-03-24 15:28:21 +0900

    리눅스로 이사감

--- src/search/container/Search.test.tsx
+++ src/search/container/Search.test.tsx
@@ -1,30 +1,33 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { Provider } from 'react-redux';
-import Search from './Search';
 // import store from '../../common/store';
-import configureStore from 'redux-mock-store';
-import { UserAction } from '../../user/state/ducks';
-import { SearchAction } from '../state/ducks';
+import { combineReducers, createStore } from 'redux';
+import mySearchReducer from '../state/ducks';
+// import configureStore from 'redux-mock-store';
+import Search from './Search';
 
 jest.mock('../component/Settings', () => () => <></>);
 // jest.mock('./SearchInput', () => () => <></>);
 
+let store: any;
+
+beforeEach(() => {
+  const reducer = combineReducers({
+    search: mySearchReducer
+  });
+  store = createStore(reducer);
+});
+
 test('shoud match the snapshot when render Search Component', () => {
   //given
-
   //when
-  const createMockStore = configureStore();
-  const component = render(
-    <Provider
-      store={createMockStore({
-        search: { keyword: '', autoCompletes: [] }
-      })}
-    >
+  const container = render(
+    <Provider store={store}>
       <Search />
     </Provider>
   );
 
   //then
-  expect(component).toMatchSnapshot();
+  expect(container).toMatchSnapshot();
 });
--- src/search/container/SearchInput.test.tsx
+++ src/search/container/SearchInput.test.tsx
@@ -1,14 +1,98 @@
-import { render } from '@testing-library/react';
+import { all } from '@redux-saga/core/effects';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
-import Search from './Search';
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import myUserReducer from '../../user/state/ducks';
+import mySearchReducer from '../state/ducks';
+import SearchInput from './SearchInput';
+import searchSaga from '../../search/state/saga';
+import userSaga from '../../user/state/saga';
 
-jest.mock('../component/Settings', () => () => <></>);
-jest.mock('./SearchInput', () => () => <></>);
-test('shoud match the snapshot when render Search Component', () => {
+// jest.mock('../../common/util/api', () =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(
+//       () =>
+//         resolve({
+//           isSuccess: true,
+//           data: [
+//             {
+//               name: 'user12',
+//               tag: 'study',
+//               department: 'dst'
+//             }
+//           ],
+//           resultCode: 200,
+//           resultMessage: 'what'
+//         }),
+//       2000
+//     );
+//   }).then(value => value)
+// );
+let store: any;
+
+beforeEach(() => {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const sagaMiddleware = createSagaMiddleware();
+  const reducer = combineReducers({
+    search: mySearchReducer,
+    user: myUserReducer
+  });
+  store = createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(sagaMiddleware))
+  );
+  function* rootSaga() {
+    yield all([searchSaga(), userSaga()]);
+  }
+  sagaMiddleware.run(rootSaga);
+});
+test('should show inputbox when rendering', () => {
   //given
+  //when
+  render(
+    <Provider store={store}>
+      <SearchInput />
+    </Provider>
+  );
+
+  //then
+  const inputbox = screen.getByRole('combobox');
+  expect(inputbox).toBeDefined();
+});
+
+test('should show changed text when change text', async () => {
+  //given
+  render(
+    <Provider store={store}>
+      <SearchInput />
+    </Provider>
+  );
 
   //when
-  const component = render(<Search />);
+  const inputbox = screen.getByRole('combobox');
+  fireEvent.change(inputbox, { target: { value: 'user1' } });
+  const text = inputbox.getAttribute('value');
+  await setTimeout(() => {}, 3000);
   //then
-  expect(component).toMatchSnapshot();
+  expect(text).toBe('user1');
+});
+
+test('should be called mock function when change text', async () => {
+  //given
+
+  render(
+    <Provider store={store}>
+      <SearchInput />
+    </Provider>
+  );
+
+  //when
+  const inputbox = screen.getByRole('combobox');
+  fireEvent.change(inputbox, { target: { value: 'user1' } });
+  const text = inputbox.getAttribute('value');
+  await setTimeout(() => {}, 3000);
+  screen.debug();
 });
